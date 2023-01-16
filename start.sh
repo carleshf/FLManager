3906357f4ee6102d95ecd859fbd774eee7c65103
Socayna Jouide <57795749+Socayna@users.noreply.github.com>
2023-01-16 17:49:46 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -8,14 +8,15 @@
 export PATH_TO_CSV=/absolute/path/to/csv/or/excel/file.csv
 export PATH_TO_CODE=/absolute/path/where/you/want/to/store/federated_learning_code
 export DATA_PATH=/absolute/path/to/data/folder/containing/images
-#export HOSPITAL_NAME=ub
-export HOSPITAL_NAME=aristotle-university-thesaloniki
-#export HOSPITAL_NAME=germans-trias-i-pujol
-#export HOSPITAL_NAME=gumed
-#export HOSPITAL_NAME=hospital-italiano-de-buenos-aires
-#export HOSPITAL_NAME=la-fe-university-and-polytechnic-hospital
-#export HOSPITAL_NAME=upenn
-#export HOSPITAL_NAME=parc-tauli
+export HOSPITAL_NAME=UB
+#export HOSPITAL_NAME=BUCH
+#export HOSPITAL_NAME=GEM
+#export HOSPITAL_NAME=ICRC
+#export HOSPITAL_NAME=KUH
+#export HOSPITAL_NAME=UCL
+#export HOSPITAL_NAME=UMCU
+#export HOSPITAL_NAME=AUMC
+#export HOSPITAL_NAME=VHIR
 
 # ----------------------------------------------------------------
 # DO NOT CHANGE THE FOLLOWING VARIABLES
