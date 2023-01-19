6dfd6e74605f7b5b335d0301bc1d38ca1f41b4c1
Socayna Jouide <57795749+Socayna@users.noreply.github.com>
2023-01-19 10:37:37 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -8,7 +8,7 @@
 export PATH_TO_CSV=/absolute/path/to/csv/or/excel/file.csv
 export PATH_TO_CODE=/absolute/path/where/you/want/to/store/federated_learning_code
 export DATA_PATH=/absolute/path/to/data/folder/containing/images
-export HOSPITAL_NAME=UB
+#export HOSPITAL_NAME=UB
 #export HOSPITAL_NAME=BUCH
 #export HOSPITAL_NAME=GEM
 #export HOSPITAL_NAME=ICRC
@@ -21,9 +21,9 @@ export HOSPITAL_NAME=UB
 # ----------------------------------------------------------------
 # DO NOT CHANGE THE FOLLOWING VARIABLES
 # ----------------------------------------------------------------
-export CENTRAL_SERVER_IP=84.88.189.135
+export CENTRAL_SERVER_IP=84.88.189.140
 export CENTRAL_SERVER_PORT=5672
-export FLOWER_CENTRAL_SERVER_IP=84.88.186.195
+export FLOWER_CENTRAL_SERVER_IP=84.88.189.140
 export FLOWER_CENTRAL_SERVER_PORT=8080
 export DOCKER_IMAGE_NAME=registry.gitlab.bsc.es/bfp/fl_breast_mg_classification
 export GITHUB_REPO_NAME=https://github.com/UBFL/FLClient.git
